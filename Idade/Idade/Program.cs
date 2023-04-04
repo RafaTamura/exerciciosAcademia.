12b9337ddba2b8ca310e834975aa8ee613c4d621
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-04 17:01:09 -0300

    Maioridade

---
+++ Idade/Idade/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a sua idade?");
+            int id = int.Parse(Console.ReadLine());
+
+            if (id >= 18) {
+                Console.WriteLine("Você é maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade");
+            }
+
+        }
+    }
+}
