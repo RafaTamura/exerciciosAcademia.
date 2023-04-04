f75bb69ee9ee076232ace2aae6d53cbd577aafc8
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-04 17:00:24 -0300

    Salario incompleto

---
+++ Salario/Salario/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //leia o número de um vendedor, o seu salário fixo, o total de vendas por ele efetuadas e o percentual
+           //que ganha sobre o total de vendas. Calcule o salário total do vendedor.Escreva o número do vendedor e o salário total
+           //do vendedor.
+            Console.WriteLine("Calcule o seu salário total! ");
+            Console.WriteLine("Digite o seu número de vendedor: ");
+            float num = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu salário fixo: ");
+            float sal = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu total de vendas: ");
+            float vend = float.Parse(Console.ReadLine());
+            Console.WriteLine("Agora digite o percentual que tu ganha sobre o total de vendas: ");
+            float porc = float.Parse(Console.ReadLine());
+
+            float total 
+
+            Console.WriteLine("O sálario total do vendedor " + num + " é de " + total)
+
+        }
+    }
+}
