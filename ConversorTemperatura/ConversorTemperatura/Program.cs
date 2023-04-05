a3f1d1bb56b0c793abd07a8fafe79e61040b3c6c
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-04 21:24:51 -0300

    Converter temperatura

---
+++ ConversorTemperatura/ConversorTemperatura/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System.Text.RegularExpressions;
+
+namespace ConversorTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit.
+          //  A fórmula da conversão é F = (9 * C + 160) / 5.
+            Console.WriteLine("Conversor de Temperatura");
+            Console.WriteLine("Insira a temperatura em Celsius");
+            int cel = int.Parse(Console.ReadLine());
+
+            int fahr = ((9 * cel + 160) / 5);
+            Console.WriteLine("A temperatura " +  cel + " Celsius, é igual a " + fahr + " Fahrenheit"); 
+        }
+    }
+}
