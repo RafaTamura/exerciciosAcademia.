5b50db836b9ece39bcebbe53d9fcb126f0941393
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-04 21:02:29 -0300

    Formato de datas

---
+++ Data/Data/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //que receba a data na forma DDMMAAAA e imprima na 
+            // forma AAAAMMDD e AAMMDD. Note que o dia, o mês e o ano devem ser lidos em variáveis diferentes.
+            Console.WriteLine( "Vamos transformar as datas em outros formatos" );
+            Console.WriteLine("Primeiro digite o dia: ");
+            int dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Agora digite o mês: ");
+            int mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Agora digite o ano: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Temos o formato " + mes + "/" + dia + "/" + ano);
+            Console.WriteLine("Temos o formato " + ano + "/" + mes + "/" + dia);
+        }
+    }
+}
