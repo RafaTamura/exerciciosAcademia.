abe31a6c3e2b67e13154cbf9e0e3544b9d4a3e58
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-08 21:54:40 -0300

    Nota Média Aprovação

---
+++ Exercícios/NotaMediaAluno/NotaMediaAluno/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NotaMediaAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escrever um algoritmo para ler duas notas de um aluno e escrever na tela a palavra “Aprovado” 
+            //se a média das duas notas for maior ou igual a 7,0. 
+
+            System.Console.WriteLine("Insira a primeira nota: ");
+            float not1 = float.Parse(Console.ReadLine());
+            System.Console.WriteLine("Insira a segunda nota: ");
+            float not2 = float.Parse(Console.ReadLine());
+
+            float media = (not1 + not2) / 2;
+            System.Console.WriteLine(media >= 7.0 ? " O aluno foi aprovado com " + media + " de média" : "O aluno foi reprovado com " + media + " de média");
+        }
+    }
+}
