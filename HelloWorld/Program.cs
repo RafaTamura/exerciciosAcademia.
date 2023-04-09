332108ec9d4989277a96e807e79af1cd43cd2449
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-08 21:15:26 -0300

    Calcular a área do triângulo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,31 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//•Dados três números, verificar se eles podem 
-//representar as medidas dos lados de um triângulo e,
- //classificar 
-//o triângulo em equilátero, isósceles ou escaleno.
-/*Console.WriteLine("Insira o primeiro número");
-int prim = int.Parse(Console.ReadLine());
-Console.WriteLine("Insira o segundo número");
-int sec = int.Parse(Console.ReadLine());
-Console.WriteLine("Insira o terceiro número");
-int terc = int.Parse(Console.ReadLine());
-//equilatero
-if (prim < sec+terc && sec < prim+terc && terc< prim + sec){
 
-if (prim == sec && sec==terc){
-Console.WriteLine("Triângulo equilátero");
-
-} else if (prim != sec && sec != terc && prim != terc){
-Console.WriteLine("Triângulo escaleno");
-
-} else {
-    Console.WriteLine("Triângulo isósceles ");
-}
-} else {
-    Console.WriteLine("Não é um triângulo");
-
-}*/
 
 // Faça um algoritmo para calcular e mostrar a área de um triângulo a partir dos valores da base e da altura
  //lidos pelo teclado: 
