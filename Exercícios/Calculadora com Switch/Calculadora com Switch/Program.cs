39eed71adb0d7d4302be0a7967a8a15e4d4e0c78
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-08 21:38:24 -0300

    Calculadora com o uso de switch

---
+++ Exercícios/Calculadora com Switch/Calculadora com Switch/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculadora_com_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculadora com Switch
+            System.Console.WriteLine("Digite a operação que deseja realizar: ");
+            string op = Console.ReadLine(); 
+            System.Console.WriteLine("Digite o primeiro número: ");
+            float num1 =float.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite o segundo número: ");
+            float num2 =float.Parse(Console.ReadLine());
+
+            switch (op){
+                case "+" :{
+                    float res = num1 + num2;  
+                    System.Console.WriteLine("O resultador é: " + res);
+                    break;
+                }
+
+                case "-":{
+                     float res = num1 - num2;  
+                    System.Console.WriteLine("O resultador é: " + res);
+                    break;
+                }
+                case "x":{
+                     float res = num1 * num2;  
+                    System.Console.WriteLine("O resultador é: " + res);
+                    break;
+                }
+                case "/":{
+                     float res = num1 / num2;  
+                    System.Console.WriteLine("O resultador é: " + res);
+                    break;
+                }
+                default :
+                    System.Console.WriteLine("Você não digitou nenhuma das operações válidas, elas são +, -, x, /");
+                    break;
+                }        }
+                }
+            }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,31 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-//Escrever um algoritmo para ler quatro valores, calcular a sua média, e escrever na tela os que são superiores à média.
-Console.WriteLine("Insira o primeiro número: ");
-float prim = float.Parse(Console.ReadLine());
-Console.WriteLine("Insira o segundo número: ");
-float sec = float.Parse(Console.ReadLine());
-Console.WriteLine("Insira o terceiro número: ");
-float terc = float.Parse(Console.ReadLine());
-Console.WriteLine("Insira o quarto número: ");
-float quart = float.Parse(Console.ReadLine());
-
-float media = ((prim+ terc+ sec+ quart)/4);
-System.Console.WriteLine("A média dos quatro números é: " + media);
-
-if (prim > media){
-    System.Console.WriteLine("O número "+ prim + " é superior a média");
-}
-if (sec > media){
-    System.Console.WriteLine("O número "+ sec + " é superior a média");
-}
-if (terc > media){
-    System.Console.WriteLine("O número "+ terc + " é superior a média");
-}
-if (quart > media){
-    System.Console.WriteLine("O número "+ quart + " é superior a média");
-}*/
 
 //2 - Faça um algoritmo para ler um número e verificar se ele é par ou ímpar. 
 /*System.Console.WriteLine("Digite um número: ");
@@ -34,42 +9,7 @@ if (num % 2 == 0) {
     System.Console.WriteLine("O número "+ num + " é par");
 } else {
     System.Console.WriteLine("O número "+ num + " é ímpar");
-}*/
-
-// Calculadora com Switch
-/*System.Console.WriteLine("Digite a operação que deseja realizar: ");
-string op = Console.ReadLine(); 
-System.Console.WriteLine("Digite o primeiro número: ");
-float num1 =float.Parse(Console.ReadLine());
-System.Console.WriteLine("Digite o segundo número: ");
-float num2 =float.Parse(Console.ReadLine());
-
-switch (op){
-    case "+" :{
-        float res = num1 + num2;  
-        System.Console.WriteLine("O resultador é: " + res);
-        break;
-    }
-
-    case "-":{
-         float res = num1 - num2;  
-        System.Console.WriteLine("O resultador é: " + res);
-        break;
-    }
-    case "x":{
-         float res = num1 * num2;  
-        System.Console.WriteLine("O resultador é: " + res);
-        break;
-    }
-    case "/":{
-         float res = num1 / num2;  
-        System.Console.WriteLine("O resultador é: " + res);
-        break;
-    }
-    default :
-        System.Console.WriteLine("Você não digitou nenhuma das operações válidas, elas são +, -, x, /");
-        break;
-    }*/
+}
 
 
 //4 - Escrever um algoritmo para ler a quantidade de horas aula dadas por dois professores e o valor por hora 
