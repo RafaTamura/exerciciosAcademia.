80739333911f530851d6e30ce8a3608426a2b2ab
Rafa Tamura <rafaelatamura@gmail.com>
2023-04-08 21:29:49 -0300

    Media de quatro numeros

---
+++ Exercícios/MediaQuatro/MediaQuatro/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MediaQuatro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escrever um algoritmo para ler quatro valores, calcular a sua média, e escrever na tela os que são superiores à média.
+            Console.WriteLine("Insira o primeiro número: ");
+                float prim = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o segundo número: ");
+                float sec = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o terceiro número: ");
+                float terc = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o quarto número: ");
+                float quart = float.Parse(Console.ReadLine());
+
+                float media = ((prim + terc + sec + quart) / 4);
+                System.Console.WriteLine("A média dos quatro números é: " + media);
+
+                    if (prim > media)
+                    {
+                        System.Console.WriteLine("O número " + prim + " é superior a média");
+                    }
+                    if (sec > media)
+                    {
+                        System.Console.WriteLine("O número " + sec + " é superior a média");
+                    }
+                    if (terc > media)
+                    {
+                        System.Console.WriteLine("O número " + terc + " é superior a média");
+                    }
+                    if (quart > media)
+                    {
+                        System.Console.WriteLine("O número " + quart + " é superior a média");
+                    }
+            }
+        }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,26 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-
-// Faça um algoritmo para calcular e mostrar a área de um triângulo a partir dos valores da base e da altura
- //lidos pelo teclado: 
-//Área do triangulo = (base * altura) / 2; 
-//Teste se a base ou a altura digitada não foi igual a zero.
-/*
-Console.WriteLine("Vamos calcular a área do triângulo!");
-Console.WriteLine("Insira a base do triângulo: ");
-float bas = float.Parse(Console.ReadLine());
-Console.WriteLine("Insira a altura do triângulo: ");
-float altura = float.Parse(Console.ReadLine());
-if (bas > 0 && altura > 0){
-float area = ((bas*altura)/2); 
-
-Console.WriteLine("A área do triângulo é igual a: " + area);
-
-} else {
-    Console.WriteLine("A base ou a altura não podem ser menores ou igual a Zero");
-}
-
 //Escrever um algoritmo para ler quatro valores, calcular a sua média, e escrever na tela os que são superiores à média.
 Console.WriteLine("Insira o primeiro número: ");
 float prim = float.Parse(Console.ReadLine());
